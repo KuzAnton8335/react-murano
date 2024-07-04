13abe2e89f317a5abcf02379514a50651ae7fa99
KuzAnton8335 <ant2783@gmail.com>
2024-07-04 16:05:00 +0300

    begin modules

---
+++ src/modules/Goods/Goods.jsx
@@ -0,0 +1,37 @@
+import { goodsArray } from "../../../goodsArray";
+import Cart from "../Cart/Cart";
+export const Goods = () => {
+	// todo:остановился на article модуле
+	return (
+		<section className="goods">
+			<div className="container goods__container">
+				<div className="goods__box">
+					<h2 className="goods__title">Цветы</h2>
+
+					<ul className="goods__list">
+						{goodsArray.map((item) => (
+							<li key={item.id} className="goods__item" >
+								<article className="goods__card card"><img className="card__image"
+									src="https://dull-rose-pawpaw.glitch.me/img/38.jpg"
+									alt="Букет из тюльпан Dolche vita (51 шт)" />
+									<div className="card__content">
+										<h3 className="card__title">Букет из тюльпан Dolche vita (51 шт)
+										</h3>
+										<div className="card__footer">
+											<p className="card__date-delivery">сегодня в 14:00</p><button
+												className="card__button">6700&nbsp;₽</button>
+										</div>
+									</div>
+								</article>
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<Cart />
+
+			</div>
+		</section >
+	)
+}
+
